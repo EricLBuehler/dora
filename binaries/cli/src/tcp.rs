2e682de0f62302a14461a54b81b4c013f2e7e7b3
Philipp Oppermann <dev@phil-opp.com>
2026-02-11 21:41:53 +0100

    Enforce max message length for legacy interface

--- binaries/cli/src/tcp.rs
+++ binaries/cli/src/tcp.rs
@@ -13,11 +13,19 @@ impl AsyncTcpConnection {
     }
 
     pub async fn receive(&mut self) -> std::io::Result<Vec<u8>> {
+        const MAX_MESSAGE_LEN: usize = 256 * 1024 * 1024; // 256 MiB
+
         let reply_len = {
             let mut raw = [0; 8];
             self.stream.read_exact(&mut raw).await?;
             u64::from_le_bytes(raw) as usize
         };
+        if reply_len > MAX_MESSAGE_LEN {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                format!("message length {reply_len} exceeds maximum of {MAX_MESSAGE_LEN} bytes"),
+            ));
+        }
         let mut reply = vec![0; reply_len];
         self.stream.read_exact(&mut reply).await?;
         Ok(reply)
